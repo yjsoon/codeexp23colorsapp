247706672527cf3dd2366c22a0f3973a1005d82a
YJ Soon <yjsoon@tinkertanker.com>
2023-06-06 10:43:20 +0800

    Added Details Screen

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Button,
+  TouchableOpacity
+} from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
+import DetailsScreen from "./components/DetailsScreen";
 
-function HomeScreen() {
-  const [colorArray, setColorArray] = useState([
-    { red: 255, green: 127, blue: 0, id: "0" },
-    { red: 0, green: 127, blue: 255, id: "1" },
-    { red: 255, green: 0, blue: 255, id: "2" },
-    { red: 0, green: 0, blue: 100, id: "3" }
-  ]);
+function HomeScreen({ navigation }) {
+  const [colorArray, setColorArray] = useState([]);
 
   function renderBlockRGB({ item }) {
     let { red, green, blue } = item;
-    return <BlockRGB red={red} green={green} blue={blue} />;
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Details");
+        }}>
+        <BlockRGB red={red} green={green} blue={blue} />
+      </TouchableOpacity>
+    );
   }
 
   function addColor() {
@@ -35,9 +45,15 @@ function HomeScreen() {
     setColorArray(newArray);
   }
 
+  function resetColors() {
+    setColorArray([]);
+  }
+
   return (
     <View style={styles.container}>
       <Button onPress={addColor} title="Add colour" />
+      <Button onPress={resetColors} title="Reset colours" />
+
       <FlatList
         style={styles.list}
         data={colorArray}
@@ -54,6 +70,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ components/DetailsScreen.js
@@ -0,0 +1,5 @@
+import { View, Text } from "react-native";
+
+export default function DetailsScreen() {
+  return <Text>Hello! This is the details screen</Text>;
+}
