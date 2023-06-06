f5fd7459590780166ee9446d307f4bd51737c617
YJ Soon <yjsoon@tinkertanker.com>
2023-06-06 11:08:22 +0800

    Finished Detail Screen

--- components/DetailsScreen.js
+++ components/DetailsScreen.js
@@ -1,10 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function DetailsScreen({ route }) {
+  const { red, green, blue } = route.params;
+  const darkMode = red + green + blue < 300 ? true : false;
+
   return (
-    <Text>
-      Hello! This is the thing you sent:
-      {route.params.red}, {route.params.green}, {route.params.blue},
-    </Text>
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: `rgb(${red}, ${green}, ${blue})`
+        }
+      ]}>
+      <Text style={{ color: darkMode ? "white" : "black" }}>Red: {red}</Text>
+      <Text style={{ color: darkMode ? "white" : "black" }}>
+        Green: {green}
+      </Text>
+      <Text style={{ color: darkMode ? "white" : "black" }}>Blue: {blue}</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
