27d5004bc0177615886c2b3c9a2cec15c69f177f
YJ Soon <yjsoon@tinkertanker.com>
2023-06-06 10:21:22 +0800

    Added FlatList and state variable

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 
 function HomeScreen() {
+  const [colorArray, setColorArray] = useState([
+    { red: 255, green: 127, blue: 0, id: "0" },
+    { red: 0, green: 127, blue: 255, id: "1" },
+    { red: 255, green: 0, blue: 255, id: "2" },
+    { red: 0, green: 0, blue: 100, id: "3" }
+  ]);
+
+  function renderBlockRGB({ item }) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
+
   return (
     <View style={styles.container}>
-      <BlockRGB red={255} green={127} blue={0} />
-      <BlockRGB red={0} green={127} blue={255} />
-      <BlockRGB red={255} green={0} blue={127} />
+      <FlatList
+        style={styles.list}
+        data={colorArray}
+        renderItem={renderBlockRGB}
+      />
     </View>
   );
 }
@@ -31,5 +45,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#ddd",
     alignItems: "center"
+  },
+  list: {
+    width: "100%"
   }
 });
